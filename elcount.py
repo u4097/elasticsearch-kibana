138514da59355e2d9eadfa78ff17273617d2e7d0
Oleg Sitnikov <oleg@home.local>
2019-10-17 17:45:03 +0300

    Fix for local configuration

--- elcount.py
+++ elcount.py
@@ -3,6 +3,7 @@ from lxml import etree
 #doc = etree.parse("book.xml")
 
 root = etree.parse(open(sys.argv[1],'r'))
-count = sum(1 for _ in root.iter("*"))
+#count = sum(1 for _ in root.iter("*"))
+count = tree.xpath("count(sys.arg[2])")
 print(count)
 
